0fd396375db2c0707d1fb22542f7c2002b866825
Muhammad Wahab Sharif <www.rex19@gmail.com>
2025-06-17 15:26:10 +0500

    containerization

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,63 +35,59 @@
 <body class>
     <div class="grad-bg no-print w-full h-4 top-0 left-0 z-50"></div>
 
-    <div class="container mx-auto">
-        <!-- Header -->
-        <header class="flex">
-            <div class="md:w-[75%] flex flex-col items-start text-left py-3 pb-8">
-                <!-- Logo -->
-                <div class="flex-shrink-0 mb-5">
-                    <img src="{{ asset('assets/images/bitbio-logo.png') }}" alt="Dashboard Logo"
-                        class="md:h-10 h-11 w-auto p-logo">
-                </div>
-
-                <!-- Content -->
-                <div class="text-left">
-                    <h1 class="md:text-[1.8rem] text-[1.6rem] mb-4 p-heading mt-3 font-bold">Cell seeding
-                        calculator
-                    </h1>
-                    <p class="no-print">
-                        This calculator helps scientists to quickly determine how much cell stock solution and culture
-                        media
-                        is needed for
-                        seeding cells in a variety of different culture vessels. By following this simple step-by-step
-                        guide,
-                        you can get your
-                        results instantly!
-                    </p>
-                </div>
-
-                <!-- Placeholder for symmetry (hidden on small) -->
-                <div class="hidden md:block flex-shrink-0 w-20"></div>
+    <!-- Header -->
+    <header class="flex container mx-auto">
+        <div class="md:w-[75%] flex flex-col items-start text-left py-3 pb-8">
+            <!-- Logo -->
+            <div class="flex-shrink-0 mb-5">
+                <img src="{{ asset('assets/images/bitbio-logo.png') }}" alt="Dashboard Logo"
+                    class="md:h-10 h-11 w-auto p-logo">
             </div>
-        </header>
 
-        <!-- Main Content -->
-        <main>
-            <div class="mx-auto">
-                @yield(section: 'content')
-            </div>
-        </main>
-
-        <!-- Footer -->
-        <footer class="bg-white mt-10 hidden md:block text-start max-w-4xl border-gray-200 pb-12">
-            <div class="mx-auto py-2 text-black">
-                <p class="pb-2 md:pb-0">
-                    Please note that this calculator has been developed by <a class="!text-black"
-                        href="https://bit.bio">bit.bio</a> and provides standard guidance. By
-                    using this tool, you agree to our cell seeding calculator - Terms and Conditions.
+            <!-- Content -->
+            <div class="text-left">
+                <h1 class="md:text-[1.8rem] text-[1.6rem] mb-4 p-heading mt-3 font-bold">Cell seeding
+                    calculator
+                </h1>
+                <p class="no-print">
+                    This calculator helps scientists to quickly determine how much cell stock solution and culture
+                    media
+                    is needed for
+                    seeding cells in a variety of different culture vessels. By following this simple step-by-step
+                    guide,
+                    you can get your
+                    results instantly!
                 </p>
-                <div class="m-auto">
-                    <p>
-                        <a class="!text-black" href="https://bit.bio">bit.bio</a> ©
-                        {{ date('Y') }}.
-                        All
-                        rights reserved.
-                    </p>
-                </div>
             </div>
-        </footer>
-    </div>
+
+            <!-- Placeholder for symmetry (hidden on small) -->
+            <div class="hidden md:block flex-shrink-0 w-20"></div>
+        </div>
+    </header>
+
+    <!-- Main Content -->
+    <main class="container mx-auto">
+        @yield(section: 'content')
+    </main>
+
+    <!-- Footer -->
+    <footer class="bg-white container mx-auto mt-10 hidden md:block text-start max-w-4xl border-gray-200 pb-12">
+        <div class="mx-auto py-2 text-black">
+            <p class="pb-2 md:pb-0">
+                Please note that this calculator has been developed by <a class="!text-black"
+                    href="https://bit.bio">bit.bio</a> and provides standard guidance. By
+                using this tool, you agree to our cell seeding calculator - Terms and Conditions.
+            </p>
+            <div class="m-auto">
+                <p>
+                    <a class="!text-black" href="https://bit.bio">bit.bio</a> ©
+                    {{ date('Y') }}.
+                    All
+                    rights reserved.
+                </p>
+            </div>
+        </div>
+    </footer>
 </body>
 
 </html>
